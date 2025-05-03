8fe7de3f2e14bed3a4ee336556dd3ba4fbf41f59
brian <brianotina20@gmail.com>
2025-05-03 17:34:20 +0300

    initialization

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,4 +2,13 @@ export default {
    plugins: {
       "@tailwindcss/postcss": {},
    },
+   content: ['./index.html', './src/**/*.{jsx, js}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ['inter', 'serif'],
+			},
+		},
+	},
 }
